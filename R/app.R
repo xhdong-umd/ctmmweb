c3676719f30da84fc964d3535fd1471dfe286f60
Xianghui Dong <xhdong@umd.edu>
2017-11-10 12:00:59 -0500

    - use markdown in roxygen - add data parameter of run()

--- R/app.R
+++ R/app.R
@@ -1,8 +1,10 @@
 #' Start ctmm web app
 #'
+#' @param data Data to be loaded with app. Could be path to csv file, .rds file; `ctmm` telemetry object/list. Default `NULL` will not load any data.
+#'
 #' @export
 #'
-app <- function() {
+app <- function(data = NULL) {
   appDir <- system.file("app", package = "ctmmweb")
   if (appDir == "") {
     stop("Could not find app directory. Try re-installing `ctmmweb`.", call. = FALSE)
